e8bd1aab1efadd4d691258b3056ec9bc0285f6b5
Stephen Walker <stephenwalker1988@gmail.com>
2011-05-06 17:09:03 -0500

    Refactored and updated game_observer

--- lib/game_observer.rb
+++ lib/game_observer.rb
@@ -1,10 +1,69 @@
 class GameObserver
   def initialize(board)
     @game_board = board
+    @sum = [0,0]
   end
 
   def has_winner?
-    score = (0..@game_board.column_dimension).inject(0) { |acc, space| acc + @game_board.board[@game_board.row_dimension][space] }
-    score == 4
+    horizontal_winner? || vertical_winner? || diagonal_winner?
+  end
+
+  def horizontal_winner?
+    for_each_cell_by_row do |row, column|
+      check_for_connect_four(row, column)
+      return true if connect_four?
+      @sum.clear if column==@game_board.column_dimension
+    end
+    false
+  end
+
+  def vertical_winner?
+    for_each_cell_by_column do |row, column|
+      check_for_connect_four(row, column)
+      return true if connect_four?
+      @sum.clear if column==@game_board.column_dimension
+    end
+    false
+  end
+
+  def diagonal_winner?
+    @sum.clear
+    (0..3).each do |row|
+      check_for_connect_four(@game_board.row_dimension - row, row)
+      return true if connect_four?
+    end
+    false
+  end
+
+  def check_for_connect_four(row, column)
+    if is_same_player?(@game_board.board[row][column])
+      @sum[1] += 1
+    else
+      reset_sums(row, column)
+    end
+  end
+
+  def connect_four?
+    @sum[1] == 4
+  end
+
+  def is_same_player?(cell_value)
+    cell_value == @sum[0] && @sum[0] != 0
+  end
+
+  def reset_sums(row, column)
+     @sum = [@game_board.board[row][column], 1]
+  end
+
+  def for_each_cell_by_row
+    (0..@game_board.row_dimension).each do |row|
+      (0..@game_board.column_dimension).each { |column| yield(row, column) }
+    end
+  end
+
+  def for_each_cell_by_column
+    (0..@game_board.column_dimension).each do |column|
+      (0..@game_board.row_dimension).each { |row| yield(row, column) }
+    end
   end
 end
--- spec/game_observer_spec.rb
+++ spec/game_observer_spec.rb
@@ -13,4 +13,45 @@ describe GameObserver do
     end
     @observer.has_winner?.should be_true
   end
+
+  it "should detect a horizontal win" do
+    (0..2).each do |space|
+      @my_board.move(space, 1)
+    end
+    @my_board.move(4, 1)
+    @observer.has_winner?.should be_false
+  end
+
+  it "should detect a horizontal win on different rows" do
+    (1..4).each do |column|
+      @my_board.move(column, column)
+      @my_board.move(column, 1)
+      @my_board.move(column, column)
+    end
+    @observer.has_winner?.should be_true
+  end
+
+  it "should detect a vertical win" do
+    (0..3).each { @my_board.move(3) }
+    @observer.has_winner?.should be_true
+  end
+
+  it "detects a diagonal win" do
+    (0..3).each do |column|
+      column.times { @my_board.move(column, -1) }
+      @my_board.move(column, 1)
+    end
+    puts ""
+    (0..@my_board.row_dimension).each do |row|
+      (0..@my_board.column_dimension).each do |column|
+        putc "["
+        putc "O" if @my_board.board[row][column] == 1
+        putc "@" if @my_board.board[row][column] == -1
+        putc " " if @my_board.board[row][column].zero?
+        putc "]"
+      end
+      puts ""
+    end
+    @observer.has_winner?.should be_true
+  end
 end
